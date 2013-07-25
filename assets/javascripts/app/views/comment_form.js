0f003d730e8be9072bea12be310b0157072c3d08
Alex McPherson <alex@quickleft.com>
2013-07-24 23:00:54 -0600

    nuke comment form view

--- assets/javascripts/app/views/comment_form.js
+++ assets/javascripts/app/views/comment_form.js
@@ -1,17 +1,3 @@
 comments.BB.views.comment_form = Backbone.View.extend({
-	render: function() {
-		this.$el.html($('#comment_form_template').html());
-	},
-	events: {
-		'submit form': 'addComment'
-	},
-	addComment: function(e) {
-		var new_comment = new comments.BB.models.comment({author: $('.author_name').val(), comment: $('.comment_body').val()})
-		e.preventDefault();
-		
-		comments.comments_collection.add(new_comment);
-		new_comment.save();
-		comments.comments_collection.renderComments();
-		return false;
-	}
+
 })
